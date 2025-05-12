bbeaf6167020113cfaef064bc15e66a6da550ed5
Sali Janjalashvili <salijanjalashvili@gmail.com>
2025-05-12 21:00:10 +0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import {Route, createBrowserRouter, createRoutesFromElements, RouterProvider} from "react-router-dom";
+import React from "react";
+import MainLayout from "./layouts/MainLayout";
+import HomePage from "./pages/HomePage";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+    </Route>
+  )
+);
+
+const App = () => {
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
+};
+
+export default App;
